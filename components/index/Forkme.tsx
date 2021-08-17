73dfd7c40c689f43647f2be6695c46d7ee304856
Ricardo Gabriel Arana Reyes Guerrero <r.arana@globant.com>
2021-08-16 22:34:46 -0500

    feat: 🎸 add sass
    add sass and image forkme response

--- components/index/Forkme.tsx
+++ components/index/Forkme.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
-import Style from '../../styles/ForkMe.module.css';
 
 
 export default function ForkMeComponent() {
@@ -7,10 +7,9 @@ export default function ForkMeComponent() {
     return (
         <div className="ForkMe">
         <a href="https://github.com/ricardo-arana/vacunomentro-peru">
-            <Image loading="lazy" width="100"
-             height="100"
-            src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png"
-            className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" />
+            <img loading="lazy"
+            src="./images/forkme.png"
+            className="image" alt="Fork me on GitHub" data-recalc-dims="1" />
             </a>
         </div>
     );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
+
 module.exports = {
   images: {
     domains: ['github.blog'],
   },
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
