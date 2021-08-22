bd890072b6f2935cc3271f50f049f2fd6b53553c
Ricardo Gabriel Arana Reyes Guerrero <r.arana@globant.com>
2021-08-22 16:29:36 -0500

    feat: 🎸 add google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,24 @@ export default function Home({data, populationPeru}) {
       name="description"           
       content="Aplicación web para poder revisar rápidamente los indicadores de vacunación del Perú.
                Tambien podemos ver un gráfico de torta que nos pemite ver en el avance de las vacunas"></meta>
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
     </Head>
     <ForkMeComponent></ForkMeComponent>
     <header className="mb-4 mt-3">
