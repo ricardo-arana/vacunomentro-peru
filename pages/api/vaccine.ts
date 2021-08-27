7ab9e3d569918bb84c9f47e2416ba92086344b3b
Ricardo Gabriel Arana Reyes Guerrero <r.arana@globant.com>
2021-08-27 11:28:10 -0500

    feat: 🎸 add population to api

--- pages/api/vaccine.ts
+++ pages/api/vaccine.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { scrape } from '../../helpers/reader';
+import { PopulationProvider } from '../../providers/population';
 require('dotenv').config();
 
 type Data = {
@@ -15,8 +16,11 @@ export default async function handler(
   const envkey = process.env.APIKEY;
 
   if(envkey === apikey) {
-    const data = await scrape('https://www.gob.pe/pongoelhombro');
-    res.status(200).json({ data });
+    let data = await scrape('https://www.gob.pe/pongoelhombro');
+    const populationProvider = new PopulationProvider()
+    const population = await populationProvider.getPopulationPeru();
+    data = {...data, totalNoBothDose: population.population};
+    res.status(200).json(data);
   } else {
     res.status(401).json({ data: { error: 'authorization'}})
   }
